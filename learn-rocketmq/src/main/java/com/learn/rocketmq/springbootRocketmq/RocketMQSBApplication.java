0b72cb9e7d8fb0fb8877ba05cbd067cd169158b1
ChenYP <18232965581@163.com>
2023-07-31 09:58:48 +0800

    feat：rocketMQ springboot

---
+++ learn-rocketmq/src/main/java/com/learn/rocketmq/springbootRocketmq/RocketMQSBApplication.java
@@ -0,0 +1,18 @@
+package com.learn.rocketmq.springbootRocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：ChenYP
+ * @date ：Created in 2020/11/28
+ * @description:
+ **/
+
+@SpringBootApplication
+public class RocketMQSBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQSBApplication.class,args);
+    }
+}
